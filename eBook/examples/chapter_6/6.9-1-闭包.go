0d877ba2b09d113a4b308a92e5f51d8ad77cdac8
harkhuang <shiyanhk@gmail.com>
2018-10-21 16:46:46 +0800

    map test

--- eBook/examples/chapter_6/6.9-1-闭包.go
+++ eBook/examples/chapter_6/6.9-1-闭包.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	// make an Add2 function, give it a name p2, and call it:
+	        // make an Add2 function, give it a name p2, and call it:
 	p2 := Add2()
-	fmt.Printf("Call Add2 for 3 gives: %v\n", p2(3))
+	         fmt.Printf("Call Add2 for 3 gives: %v\n", p2(3))
 	// make a special Adder function, a gets value 2:
 	TwoAdder := Adder(2)
 	fmt.Printf("The result is: %v\n", TwoAdder(3))
